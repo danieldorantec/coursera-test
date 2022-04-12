b142e245e9cde141ead8d5087a9daa5d5284d659
Daniel Dorante KPI <ddorante@kpiservices.es>
2022-04-12 09:57:45 +0200

    correccion para tener en cuenta los tipos del objeto

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -51,17 +51,19 @@ WARNING!!! WARNING!!!
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-    var firstLetter = names[name].charAt(0).toLowerCase();
+    if (typeof names[name] === 'string') {
+      var firstLetter = names[name].charAt(0).toLowerCase();
 
-    // STEP 12:
-    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-    // name in the loop.
-    if (firstLetter === "j") {
-      byeSpeaker.speak(names[name]);
-    } else {
-      helloSpeaker.speak(names[name])
+      // STEP 12:
+      // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+      // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+      // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+      // name in the loop.
+      if (firstLetter === "j") {
+        byeSpeaker.speak(names[name]);
+      } else {
+        helloSpeaker.speak(names[name])
+      }
     }
   }
 
